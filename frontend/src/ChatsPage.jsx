f32ab17efc2f6c52aaf56323b2686606c036a0e6
Akash <akashchauhan84590@gmail.com>
2024-01-21 21:01:21 +0530

    first commit

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '355dd746-3865-4d00-ad87-3121076fc058', 
+        props.user.username, 
+        props.user.secret
+        );
+    return (
+    <div style={{height : '100vh'}}>
+        <MultiChatSocket {...chatProps}/>
+        <MultiChatWindow {...chatProps} style={{height : '100%'}}/>
+    </div>
+    )
 }
 
 export default ChatsPage
